5f51cd8f1731838e4d8a16fcf7b7b02644f71ef7
demon00 <dfasterdriver@yandex.ru>
2017-03-05 17:03:47 +0300

    add directory

---
+++ public/js/phone-gallary.js
@@ -0,0 +1,23 @@
+"use strict";
+
+class PhoneGallary {
+  constructor(options) {
+    this._el = options.el;
+
+    this._phone = phoneFromServ || options.phone;
+
+    this._render();
+
+  }
+
+  _render() {
+    let template = document.querySelector('#phone-gallary-template').innerHTML;
+    let compiled = _.template(template);
+
+    this._el.innerHTML = compiled({
+      phone: this._phone
+    });
+
+  }
+
+}
